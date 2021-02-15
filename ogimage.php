a3264128dab5852c0eaaaa854b3dc2fd47d8ef0b
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-02-15 12:07:32 +0200

    Plugin skeleton

---
+++ ogimage.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Lucid Fox Open Graph Image generator
+ *
+ * @package   ogimages
+ * @copyright Copyright 2021-2021 Lucid Fox
+ * @license   GNU GPL v3 or later
+ *
+ */
+
+use Joomla\CMS\Plugin\CMSPlugin;
+
+class plgSystemOgimages extends CMSPlugin
+{
+
+}
